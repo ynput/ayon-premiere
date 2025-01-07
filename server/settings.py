2c05f2b23c85cf9bac97897c748eb6b018196633
Sponge <Jackp.bcot@gmail.com>
2025-01-07 10:36:20 +0000

    draft: init of settings

--- server/settings.py
+++ server/settings.py
@@ -1,7 +1,22 @@
-from ayon_server.settings import BaseSettingsModel
+from ayon_server.settings import BaseSettingsModel, SettingsField
 
 DEFAULT_VALUES = {}
 
 
 class MySettings(BaseSettingsModel):
     pass
+
+
+class InstallAyonExtensionSettingsModel(BaseSettingsModel):
+    exman_path: str = SettingsField("", title="Path to ExManCmd executable")
+    enabled: bool = SettingsField(False, title="Enabled")
+
+
+class HooksModel(BaseSettingsModel):
+    InstallAyonExtensionToPremiere: InstallAyonExtensionSettingsModel = (
+        SettingsField(
+            default_factory=InstallAyonExtensionSettingsModel,
+            title="Install Extension",
+            description="Installs the AYON extension using the supplied ExManCmd on startup.",
+        )
+    )
