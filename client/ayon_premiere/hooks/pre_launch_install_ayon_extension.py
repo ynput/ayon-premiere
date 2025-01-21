450ab0ab4622670ac983cd3e126234939a9f130d
Petr Kalis <petr.kalis@gmail.com>
2025-01-21 13:18:59 +0100

    Added check for extension version and redeploy

--- client/ayon_premiere/hooks/pre_launch_install_ayon_extension.py
+++ client/ayon_premiere/hooks/pre_launch_install_ayon_extension.py
@@ -1,5 +1,7 @@
 import os
+import re
 import platform
+import shutil
 from zipfile import ZipFile
 
 from ayon_premiere import PREMIERE_ADDON_ROOT
@@ -46,18 +48,18 @@ class InstallAyonExtensionToPremiere(PreLaunchHook):
             os.environ["appdata"], r"Adobe\CEP\extensions\io.ynput.PPRO.panel"
         )
 
-        if os.path.exists(target_path):
-            self.log.info(
-                f"The extension already exists at: {target_path}. Cancelling.."
-            )
-            return
-
         extension_path = os.path.join(
             PREMIERE_ADDON_ROOT,
             r"api\extension.zxp",
         )
 
         try:
+            deploy = self._deploy_extension(extension_path, target_path)
+
+            if not deploy:
+                self.log.debug(f"Extension already deployed at {target_path}.")
+                return
+
             self.log.debug(f"Creating directory: {target_path}")
             os.makedirs(target_path, exist_ok=True)
 
@@ -74,3 +76,30 @@ class InstallAyonExtensionToPremiere(PreLaunchHook):
 
         except Exception as error:
             self.log.warning(f"An unexpected error occured: {error}")
+
+
+    def _deploy_extension(self, extension_path, target_path):
+        """Check if current extension version is installed, purge old ones."""
+        with ZipFile(extension_path, 'r') as zip_file:
+            with zip_file.open('CSXS/manifest.xml') as manifest_file:
+                content = manifest_file.read()
+                pattern = r'ExtensionBundleVersion="([^"]+)"'
+                extension_version = re.search(pattern, str(content)).group(1)
+
+        deployed_manifest_path = os.path.join(
+            target_path, "CSXS", "manifest.xml")
+        if not os.path.exists(deployed_manifest_path):
+            return True
+
+        with open(deployed_manifest_path, "r") as deployed_manifest_file:
+            deployed_content = deployed_manifest_file.read()
+            deployed_extension_version =(
+                re.search(pattern, str(deployed_content)).group(1)
+            )
+
+        if extension_version != deployed_extension_version:
+            self.log.info(f"Purging {target_path} because different version")
+            shutil.rmtree(target_path)
+            return True
+
+        return False
