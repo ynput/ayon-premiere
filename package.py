3f9c189e4d17f9b44517a2a8001f32e6ea67f611
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-07-31 15:54:32 +0200

    allow per project addon version

--- package.py
+++ package.py
@@ -3,6 +3,7 @@ title = "Premiere"
 version = "0.1.5+dev"
 app_host_name = "premiere"
 client_dir = "ayon_premiere"
+project_can_override_addon_version = True
 
 ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
